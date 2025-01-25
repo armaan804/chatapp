87e875caf17760ffc54993290252927fa1bf1b95
armaan804 <armaan29804@gmail.com>
2025-01-26 00:10:53 +0530

    run build

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use("/user", userroute);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
 });
 
 server.listen(port, () => {
